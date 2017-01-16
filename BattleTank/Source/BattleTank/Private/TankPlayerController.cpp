83394578bb61647815018b8089941c0474f01e98
Aaron <aaron.mclain@mavs.uta.edu>
2017-01-16 16:56:20 -0600

    Finding Screen Pixel Coordinates

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -36,7 +36,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	
 	if (GetSightRayHitLocation(HitLocation))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
 		// TODO Tell controlled tank to aim at location
 	}
 }
@@ -44,11 +44,12 @@ void ATankPlayerController::AimTowardsCrosshair()
 // Get World Location of linetrace through crosshair, true if hits landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
 {	
-	HitLocation = FVector(1.0);
-	// If we hit landscape
-		// Update HitLocation
-		// Return true
-	// Else
-		// Return false
+	// Find the crosshair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+
+	// "De-project" the screen position of the crosshair to a world direction
+	// Line-trace along that look direction, and see what we hit (up to max range)
 	return true;
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -14,16 +14,22 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 	ATank* GetControlledTank() const;	// const means reference connot alter controlled tank
 	
 	virtual void BeginPlay() override;	// virtual means children can override (append) function
 										// BeginPlay() is an AActor method
-	virtual void Tick(float DeltaTime) override;
-	
-private:
+	virtual void Tick(float DeltaTime) override;	
+
 	void AimTowardsCrosshair();	// Moves barrel to crosshair
 
 	// Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& HitLocation) const; // return location of where we would hit in landscape
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.3333f;
+
 };
